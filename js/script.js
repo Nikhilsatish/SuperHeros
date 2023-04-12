c83f5535c191dcfdd369aae75199284f7966ec85
Nikhil <nikhilsatish007@gmail.com>
2023-04-12 20:27:42 +0530

    creating hash and calling api

---
+++ js/script.js
@@ -0,0 +1,15 @@
+async function searchHeros(textSearched) {
+
+    let PUBLIC_KEY = "ad5e51c27bbb0a2dde347af8c006720f";
+    let PRIVATE_KEY = "d964140bf3a5424ba8fc867be24a3413a4279e99";
+
+    let ts = new Date().getTime();
+    let hash = CryptoJS.MD5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
+    console.log(`hash key = ${hash}`); //9dd1787d12a73b583bfacdfe70955001
+
+
+    // API call to get the data 
+    await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${PUBLIC_KEY}&hash=9dd1787d12a73b583bfacdfe70955001&nameStartsWith=${textSearched}`)
+    .then(res => res.json()) //Converting the data into JSON format
+    .then(data => console.log(data.data.results)) 
+}
